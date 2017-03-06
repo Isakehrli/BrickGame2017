a272a37bfd2feeab2e1b07b61e855419dbda5870
Unknown <kehrlii1@hawkmail.newpaltz.edu>
2017-03-06 10:49:38 -0500

    game over and lives

--- Assets/Standard Assets/2D/Scripts/ball.cs
+++ Assets/Standard Assets/2D/Scripts/ball.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class ball : MonoBehaviour {
 
     public Vector2 startingVelocity = new Vector2(15, -20);
+    private Vector3 startingPosition;
+    public GameObject gameOver;
+
+    int lives = 3;
 
     void Start() {
+        startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
     }
 	void Update () {
 		if(transform.position.y < -2.5f) {
                 GetOut();
-            }
+        }
+        if (Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
 	}
      void GetOut()
         {
             Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+        if (lives == 0) {
+            DoGameOver();
         }
+        }
+
+    void DoGameOver()
+    {
+        gameOver.SetActive(true);
+    }
 }
