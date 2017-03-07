2c9e187459d9bdc1b96d1cc18ebe4547b7fec451
Unknown <kehrlii1@hawkmail.newpaltz.edu>
2017-03-06 20:25:50 -0500

    added hits

--- Assets/Standard Assets/2D/Scripts/Brick.cs
+++ Assets/Standard Assets/2D/Scripts/Brick.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-
+    int hits = 2;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,8 @@ public class Brick : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D coll)
     {
+        hits = hits - 1;
+        if(hits == 0)
         gameObject.SetActive(false);
     }
 }
