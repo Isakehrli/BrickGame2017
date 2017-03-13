126183d5b50ec5651023ffe67e2b657b0d0a0962
Unknown <kehrlii1@hawkmail.newpaltz.edu>
2017-03-13 10:46:06 -0400

    added lives value and points value

--- Assets/Standard Assets/2D/Scripts/ball.cs
+++ Assets/Standard Assets/2D/Scripts/ball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ball : MonoBehaviour {
 
@@ -8,12 +9,17 @@ public class ball : MonoBehaviour {
     private Vector3 startingPosition;
     public GameObject gameOverSign;
     public GameObject youWinSign;
+    public Text livesValue;
+    public Text pointsValue;
 
     int lives = 3;
+    int points = 0;
 
     void Start() {
         startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
+
+        livesValue = GameObject.Find("LivesValue").GetComponent<Text>();
     }
 	void Update () {
 		if(transform.position.y < -2.5f) {
@@ -21,12 +27,14 @@ public class ball : MonoBehaviour {
         }
         if (Input.GetButtonDown("Jump")) {
             GetComponent<Rigidbody2D>().velocity = startingVelocity;
+            livesValue.text = lives.ToString();
         }
 	}
      void GetOut()
         {
             Debug.Log("You are out");
         lives = lives - 1;
+        livesValue.text = lives.ToString();
 
         transform.position = startingPosition;
         GetComponent<Rigidbody2D>().velocity = new Vector2();
@@ -42,6 +50,8 @@ public class ball : MonoBehaviour {
 
     public void YouBrokeABrick()
     {
+        points += 1;
+        pointsValue.text = points.ToString();
         var bricksLeft = FindObjectsOfType<Brick>().Length;
         if(bricksLeft == 0) {
             youWinSign.SetActive(true);
